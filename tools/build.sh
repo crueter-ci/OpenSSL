f7e16348419b0447b1a7692cf143fbcca076737b
crueter <crueter@eden-emu.dev>
2025-11-12 13:41:10 -0500

    oops
    Signed-off-by: crueter <crueter@eden-emu.dev>

--- tools/build.sh
+++ tools/build.sh
@@ -92,13 +92,13 @@ copy_build_artifacts() {
 }
 
 ## Cleanup ##
-# rm -rf "$BUILD_DIR" "$OUT_DIR"
-# mkdir -p "$BUILD_DIR" "$OUT_DIR"
+rm -rf "$BUILD_DIR" "$OUT_DIR"
+mkdir -p "$BUILD_DIR" "$OUT_DIR"
 
 ## Download + Extract ##
-# download
+download
 cd "$BUILD_DIR"
-# extract
+extract
 
 ## Configure ##
 cd "$DIRECTORY"
