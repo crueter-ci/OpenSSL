a3552226c40ad43ae7aa7119233ad7042a037629
crueter <crueter@eden-emu.dev>
2026-08-22 20:40:03 -0400

    Fix android
    Signed-off-by: crueter <crueter@eden-emu.dev>

--- tools/configure.sh
+++ tools/configure.sh
@@ -26,7 +26,7 @@ configure() {
 	    ./Configure android-"${ANDROID_ARCH}" no-asm no-shared no-makedepend --release threads no-tests \
 			-D__ANDROID_API__="${ANDROID_API}" \
 			no-docs enable-camellia enable-ec enable-ec2m enable-sm2 enable-srp enable-idea enable-mdc2 enable-rc5 enable-rfc3779 enable-asm \
-			enable-quic enable-fips --prefix=/ --libdir=lib no-apps
+			enable-quic enable-fips --prefix="$PWD/../out" --libdir=lib no-apps
 	else
 		./Configure "$CONFIGURE_TARGET" no-asm no-shared no-makedepend --release threads no-tests \
 			no-docs enable-camellia enable-ec enable-ec2m enable-sm2 enable-srp enable-idea enable-mdc2 enable-rc5 enable-rfc3779 enable-asm \
