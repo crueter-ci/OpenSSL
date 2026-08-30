a1b1967f00668d9353a2cbe0849344059efe8e31
crueter <crueter@eden-emu.dev>
2026-08-30 00:55:06 -0400

    Fix pakcage
    Signed-off-by: crueter <crueter@eden-emu.dev>

--- tools/package.sh
+++ tools/package.sh
@@ -11,7 +11,7 @@ ROOTDIR="$PWD"
 copy_build_artifacts() {
 	_group "Copying build artifacts"
 
-	make install -C "$DIRECTORY"
+	"${MAKE:-make}" install -C "$DIRECTORY"
 	rm -rf out/lib/cmake out/lib/ossl-modules out/lib/pkgconfig out/ssl out/bin
 	cp cert.h out/include/openssl
 
